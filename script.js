b18ec7f2ccbc1f76950983efe61c6f79d91fa040
Courtney Cash <cashcourtney97@gmail.com>
2021-07-15 12:41:17 -0400

    Javascript added

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-document.querySelector('body').appendChild(h2); 
+function moreAboutFrisbee() {
+     alert ("Ultimate Frisbee is a game where you have to catch the disc within the endzone. There are seven people on the field at once per team. When you catch the disc you have to stop running and then have ten seconds to throw the disc.")
+}
 
+function alfie() {
+     alert ("Alfie is a very energentic cat that loves attention. He is very food motivated and screams about two hours before meal time for food. I adopted Alfie two months before I adopted Ziggy. They are litter mates!") 
+}
+
+function ziggy() {
+     alert("Ziggy is a shy boy who secretly loves cuddles. Once he warms up to you he will love you forever. He is not as rambunctous as his brother but he is always looking to snuggle up next to you. ")
+}
